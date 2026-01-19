ebaab8f8eed47078dbf3ba769da968b7dcecf91d
Sayzzi <zarunixopl@hotmail.com>
2026-01-19 14:44:07 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
-import {LoginComponent} from "./features/components/loginComponent";
+import {LoginComponent} from "./features/note/components/loginComponent.tsx";
 
 function App() {
 
